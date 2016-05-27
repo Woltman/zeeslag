9fe9f2cd53ecdb307161fb0330c4b1d2789c21e7
Thom <twoltman@avans.nl>
2016-05-27 23:53:29 +0200

    het is een troepje geworden

---
+++ src/apiService.js
@@ -0,0 +1,58 @@
+(function(zeeslag) {
+    function ApiService(token){
+        this.token = token;
+    }
+    zeeslag.ApiService = ApiService;
+    
+    ApiService.prototype.printtoken = function(){
+        console.log(this.token);    
+    }
+     
+    ApiService.prototype.getGames = function(error, success){
+        return $.ajax({
+            url: "https://zeeslagavans.herokuapp.com/users/me/games",
+            data: { token: this.token },
+            success: success,
+            error: error
+        });   
+    }
+    
+    ApiService.prototype.newGameAI = function (error, success) {
+        return $.ajax({
+            url: "https://zeeslagavans.herokuapp.com/games/ai",
+            data: { token: this.token },
+            success: success,
+            error: error
+        });
+    }
+    
+    ApiService.prototype.newGame = function (error, success) {
+        return $.ajax({
+            url: "https://zeeslagavans.herokuapp.com/games",
+            data: { token: this.token },
+            success: function (data) {
+
+                if (data.error) {
+                    error(data);
+                }
+                else {
+                    success(data);
+                }
+            },
+            error: error
+            
+        });
+    }
+    
+    ApiService.prototype.getUser = function (error, succes) {
+        return $.ajax({
+        url: "https://zeeslagavans.herokuapp.com/users/me/info",
+        data: { token: this.token },
+        success: succes,
+        error: error
+    });
+    }
+        
+})(window.zeeslag = window.zeeslag || {});
+
+ 
